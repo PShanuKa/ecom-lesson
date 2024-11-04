e7548dc309c727e5527351ba52c3858d537c2a53
Pasindu shanuka <143288490+PShanuKa@users.noreply.github.com>
2024-11-04 13:39:51 +0530

    user all route and controller startcher create

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,3 +4,20 @@
 export const createUser = (req, res) => {
     res.send("create user")
 }
+
+
+export const loginUser = (req, res) => {
+    res.send("login user")
+}
+
+
+export const getUser = (req, res) => {
+    res.send("get user")
+}
+
+
+export const updateUser = (req, res) => {
+    res.send("update user")
+}
+
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, getUser, loginUser, updateUser } from "../controllers/userController.js";
 
 
 const userRouter = express.Router();
 
+//  Post Route
+userRouter.post("/create", createUser)
 
-userRouter.get("/create", createUser)
+// Login Route
+userRouter.post("/login", loginUser)
+
+// Get Route
+userRouter.get("/get", getUser)
+
+// Put Route
+userRouter.put("/update",updateUser)
 
 
 export default userRouter
